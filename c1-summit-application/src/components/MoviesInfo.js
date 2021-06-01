4a9c103931bd8993b5c193a06f372cfbde518810
Phillip Palanca <palancapg@mymail.vcu.edu>
2021-06-01 10:55:09 -0400

    Added some more code into MoviesInfo.js for the API Key that will be used to grab data from. Key is not implemented yet, but will be so in future commits so that proper funcationality testing can occur.

--- c1-summit-application/src/components/MoviesInfo.js
+++ c1-summit-application/src/components/MoviesInfo.js
@@ -10,7 +10,17 @@ function MoviesInfo(){
     function handleSubmit(e){
         e.preventDefault()
         async function fetchMyAPI(){
+            const OMDB_API_KEY = process.env.REACT_APP_API_KEY
+
+            let response = await fetch("http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=)" + new URLSearchParams({query: query}))
+            response = await response.json()
+            console.log(response.Movies)
+            setMovies(response.Movies)
             
-        }
+        } 
+       fetchMyAPI()
+       setShowMovies(true)
+       setQuery("") 
+
     }
 }
