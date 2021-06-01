3c2cb272a1975f0fbab445458168f3a9834992dc
Phillip Palanca <palancapg@mymail.vcu.edu>
2021-05-31 22:03:04 -0400

    Added variables and got started to the MoviesInfor.js

---
+++ c1-summit-application/src/components/MoviesInfo.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './MoviesInfo.css';
+import Movies from './Movies';
+
+function MoviesInfo(){
+    const [movies, setMovies] = useState([])
+    const [query, setQuery] = useState("")
+    const [showMovies,setShowMovies] = useState(false)
+
+    function handleSubmit(e){
+        e.preventDefault()
+        async function fetchMyAPI(){
+            
+        }
+    }
+}
