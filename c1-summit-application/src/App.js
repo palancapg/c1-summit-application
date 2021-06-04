2e326d0743b0bee56ba32d2e374e9569c2fe31df
Phillip Palanca <palancapg@mymail.vcu.edu>
2021-06-03 22:06:10 -0400

    Working on fetching data from API to populate table! Also simplified the fetch statement to something that is more understandable from the programmer's capabilities

--- c1-summit-application/src/App.js
+++ c1-summit-application/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesInfo from './components/MoviesInfo';
 
 function App() {
   return (
     <div className="App">
       <Header title="Movie Info"></Header>
+      <MoviesInfo></MoviesInfo>
       <Footer title="2021"></Footer>
     </div>
   );
